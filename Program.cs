32254577dc7969fbadd63e81d06ee684ae54c6f4
Ahmed Shehata <55981057+Eng-AhmedShehata@users.noreply.github.com>
2020-02-23 20:05:37 +0200

    review & update some little things.
    </Good job bro>

--- Program.cs
+++ Program.cs
@@ -7,12 +7,16 @@ namespace SimpleForm
 {
 	class Program
 	{
+		// Avoid to use static member as you can
 		static string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
 		static void Main(string[] args)
 		{
+			// Try to order your code, by calling methods in main method 
+			// and call them in a correct way like this 
+			// printWelcomeMess(); so this method contain a code that perform an action as it called..and so on.
+			
 			Console.WriteLine("Welcome To Our Simple Form \n");
 			Console.WriteLine("for Sign Up press 0 & Sign In 1");
-
 			string signOperation = Console.ReadLine();
 
 			while (signOperation != "0" && signOperation != "1")
@@ -22,14 +26,12 @@ namespace SimpleForm
 			}
 
 			int operation = Convert.ToInt32(signOperation);
-
 			if (operation == (int)Operation.signUp)
 			{
-
 				Console.WriteLine("Please enter your username");
-
 				string userName;
 				int userFound;
+				
 				do
 				{
 					userName = Console.ReadLine();
@@ -63,27 +65,29 @@ namespace SimpleForm
 					}
 
 				} while (key.Key != ConsoleKey.Enter || password.Length < 8);
+				
 				Console.WriteLine();
-
 				Console.WriteLine("Please select your gender");
 				Console.WriteLine("for male select m for female select f");
-
+				// Get gender type
 				string gender = Console.ReadLine();
 				gender = gender.ToUpper();
-
+				
+				// Make a loop to get correct gender type
 				while(gender != "M" && gender != "F")
 				{
 					Console.WriteLine("Please Enter m or f");
 					gender = Console.ReadLine();
 					gender = gender.ToUpper();
 				}
-
+				
+				// Open sql connection then insert data into db. 
 				using (SqlConnection sqlConnection = new SqlConnection(CS))
 				{
-					SqlCommand cmd = new SqlCommand("INSERT INTO college.dbo.student VALUES (@username,@password,@gender)",sqlConnection);
-					cmd.Parameters.AddWithValue("@username",userName);
-					cmd.Parameters.AddWithValue("@password",password);
-					cmd.Parameters.AddWithValue("@gender",gender);
+					SqlCommand cmd = new SqlCommand("INSERT INTO college.dbo.student VALUES (@username, @password, @gender)", sqlConnection);
+					cmd.Parameters.AddWithValue("@username", userName);
+					cmd.Parameters.AddWithValue("@password", password);
+					cmd.Parameters.AddWithValue("@gender", gender);
 
 					sqlConnection.Open();
 					cmd.ExecuteNonQuery();
@@ -107,13 +111,14 @@ namespace SimpleForm
 						key = Console.ReadKey(true);
 						if (key.Key != ConsoleKey.Enter)
 						{
-							password += key.KeyChar;
+							password += key.KeyChar;	
 							Console.Write("*");
 						}
 
 					} while (key.Key != ConsoleKey.Enter || password.Length < 8);
 					Console.WriteLine();
-
+					
+					// Get data from db 
 					using (SqlConnection sqlConnection = new SqlConnection(CS))
 					{
 						SqlCommand cmd = new SqlCommand("SELECT * FROM college.dbo.student WHERE username = @username AND password = @password", sqlConnection);
@@ -123,9 +128,9 @@ namespace SimpleForm
 						sqlConnection.Open();
 						userFound = Convert.ToInt32(cmd.ExecuteScalar() ?? 0);
 					}
-					if(userFound == 0)
+					if (userFound == 0) {
 						Console.WriteLine("Invalid username or password");
-
+					}
 
 				} while (userFound == 0);
 
@@ -133,7 +138,7 @@ namespace SimpleForm
 			}
 			else
 			{
-				Console.WriteLine("Error ocurred , Please try again later");
+				Console.WriteLine("Error ocurred, Please try again later");
 			}
 		}
 
